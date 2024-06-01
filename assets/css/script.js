5078f3a9856594203a093fadb99dc422e320bbe5
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-06-01 07:42:18 +0200

    Finita animazione sulla navbar e fixato  una prima  parte del main

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,9 +1,21 @@
 window.addEventListener("scroll", function () {
   const navbar = document.getElementById("barraNav");
+  const button = document.getElementsByClassName("buttonNav");
+  const buttonStarter = document.getElementsByClassName("getStarter");
   const scrollPosition = window.scrollY;
-  if (scrollPosition > 400) {
+  if (scrollPosition > 260) {
     navbar.classList.add("scrolled");
+    button[0].classList.add("scrolled");
+    button[1].classList.add("scrolled");
+    button[2].classList.add("scrolled");
+    button[3].classList.add("scrolled");
+    buttonStarter[0].classList.add("scrolled2");
   } else {
     navbar.classList.remove("scrolled");
+    button[0].classList.remove("scrolled");
+    button[1].classList.remove("scrolled");
+    button[2].classList.remove("scrolled");
+    button[3].classList.remove("scrolled");
+    buttonStarter[0].classList.remove("scrolled2");
   }
 });
